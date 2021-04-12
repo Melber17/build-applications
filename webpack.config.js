be27e7363acb523e63bf9b6b2b980dc97d45665b
Evgeniy <melber1727@mail.ru>
2021-04-12 22:16:17 +0300

    finished build

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,80 @@
-module.exports = {
-	mode: "development",
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: [{ loader: "babel-loader" }],
-			},
-			{
-				test: /\.(png|jpg|jpeg|gif|ico)$/,
-				use: [
-					{
-						loader: "file-loader",
-						options: {
-							outputPath: "img",
-							name: "[name]-[sha1:hash:7].[ext]",
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = (env = {}) => {
+	const { mode = "development" } = env;
+	const isProd = mode === "production";
+	const isDev = mode === "development";
+
+	const getStyleLoaders = () => {
+		return [isProd ? MiniCssExtractPlugin.loader : "style-loader", "css-loader"];
+	};
+	const getPlugins = () => {
+		const plugins = [
+			new HtmlWebpackPlugin({
+				title: "Build",
+				buildTime: new Date().toISOString(),
+				template: "public/index.html",
+			}),
+		];
+
+		if (isProd) {
+			plugins.push(new MiniCssExtractPlugin({
+				filename: 'main-[hash:8].css'
+			}))
+		}
+	}
+	return {
+		mode: isProd ? "production" : isDev && "development",
+		output: {
+			filename: isProd ? 'main-[hash:8].js' : undefined
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					use: [{ loader: "babel-loader" }],
+				},
+				{
+					test: /\.(png|jpg|jpeg|gif|ico)$/,
+					use: [
+						{
+							loader: "file-loader",
+							options: {
+								outputPath: "img",
+								name: "[name]-[sha1:hash:7].[ext]",
+							},
 						},
-					},
-				],
-			},
-			{
-				test: /\.(ttf|otf|woff|woff2)$/,
-				use: [
-					{
-						loader: "file-loader",
-						options: {
-							outputPath: "fonts",
-							name: "[name].[ext]",
+					],
+				},
+				{
+					test: /\.(ttf|otf|woff|woff2)$/,
+					use: [
+						{
+							loader: "file-loader",
+							options: {
+								outputPath: "fonts",
+								name: "[name].[ext]",
+							},
 						},
-					},
-				],
-			},
-			{
-				test: /\.(css)$/,
-				use: [ "style-loader","css-loader"],
-			},
-			{
-				test: /\.(s[ca]ss)$/,
-				use: [
-					{ loader: "style-loader" },
-					{ loader: "css-loader" },
-					{ loader: "sass-loader" },
-				],
-			},
-		],
-	},
+					],
+				},
+				{
+					test: /\.(css)$/,
+					use: getStyleLoaders(),
+				},
+				{
+					test: /\.(s[ca]ss)$/,
+					use: [
+						...getStyleLoaders(),
+						{ loader: "sass-loader" },
+					],
+				},
+			],
+		},
+		plugins: getPlugins(),
+		devServer: {
+			open: true,
+		},
+	};
 };
